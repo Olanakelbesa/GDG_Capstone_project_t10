a58b57e836d67c7d3c98d246e370a5aceb2ac53c
dawit2-4 <dawitbirhanu363@gmail.com>
2025-04-10 01:05:28 +0300

    pulling files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
-import Front from "./components/Front";
-function App() {
+// import Front from "./components/Front";
+// function App() {
+//   return (
+//     <div>
+//       <Front />
+//     </div>
+//   );
+// }
+
+// export default App;
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+
+const App = () => {
   return (
-    <div>
-      <Front />
-    </div>
+    <Router>
+      <Routes>
+        {/* Home route */}
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
