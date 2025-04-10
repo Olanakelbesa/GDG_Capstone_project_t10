cff3b34400888ad029d2a445749dd957c6c57c33
Bewnet <bewnetaddisalem@gmail.com>
2025-04-10 23:06:11 +0300

    The route have been updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Package from "./pages/Package";
+import Singleblog from "./pages/Singleblog";
+
+
 
 const App = () => {
   return (
@@ -8,6 +12,9 @@ const App = () => {
       <Routes>
         {/* Home route */}
         <Route path="/" element={<Home />} />
+        <Route path="/package" element={<Package />} />
+        <Route path="/blog" element={<Singleblog />} />
+        
       </Routes>
     </Router>
   );
