bd744e31bf9ad8a1df9ca1de2697724ae3d99b33
Abenezer daniel <abenezerdaniel06@gmail.com>
2025-04-13 18:06:34 +0300

    feat: added some hover effect on category box and recent post

--- src/components/common/Category_box.jsx
+++ src/components/common/Category_box.jsx
@@ -14,7 +14,7 @@ function CategoryBox() {
         <div className="rounded w-full shadow-lg mx-14 my-14 mr-0">
           <h3 className="text-left font-bold mt-3 mb-3 text-4xl">Categories</h3>
 
-          <div className="flex items-center rounded hover:bg-gray-300 transform hover:scale-110 transition duration-150">
+          <div className="flex items-center rounded hover:bg-gray-300 hover:cursor-pointer transform hover:scale-110 transition duration-150">
             <FaArrowRight className="m-5" />
             <p className="m-3">Travel</p>
           </div>
@@ -22,19 +22,18 @@ function CategoryBox() {
           <hr className="opacity-15" />
 
           <div
-            className="flex items-center rounded hover:bg-gray-300 transform hover:scale-110 transition duration-150 cursor-pointer"
-          >
+            className="flex items-center rounded hover:bg-gray-300 hover:cursor-pointer transform hover:scale-110 transition duration-150 cursor-pointer">
             <FaArrowRight className="m-5" />
             <p className="m-3">Tips</p>
           </div>
 
           <hr className="opacity-15" />
-          <div className="flex items-center rounded hover:bg-gray-300 transform hover:scale-110 transition duration-150" onClick={scrollToStory}>
+          <div className="flex items-center rounded hover:bg-gray-300 hover:cursor-pointer transform hover:scale-110 transition duration-150" onClick={scrollToStory}>
             <FaArrowRight className="m-5" />
             <p>Stories</p>
           </div>
           <hr className="opacity-15" />
-          <div className="flex items-center rounded hover:bg-gray-300 transform hover:scale-110 transition duration-150">
+          <div className="flex items-center rounded hover:bg-gray-300 hover:cursor-pointer transform hover:scale-110 transition duration-150">
             <FaArrowRight className="m-5" />
             <p>Destination</p>
           </div>
--- src/components/common/Recent_posts.jsx
+++ src/components/common/Recent_posts.jsx
@@ -4,10 +4,10 @@ function Recent_posts(props) {
 
 
   return (
-    <div className="flex flex-col gap-4 p-4 bg-gray-100 rounded overflow-hidden hover:shadow-2xl transition-shadow duration-300 ease-in m-3"> 
+    <div className="flex flex-col gap-4 p-4 bg-gray-100 rounded  hover:bg-amber-100 transform hover:scale-95 transition duration-150"> 
       
        <div className="flex w-full h-full  ">
-          <img src={props.img} alt="RECENT POSTS" className="float-left rounded overflow-hidden pl-0  pr-6  w-full h-32 object-cover sm:w-1/2 sm:h-24 md:w-1/3 md:h-28 lg:w-full"/>
+          <img src={props.img} alt="RECENT POSTS" className="float-left rounded pl-0  pr-6  w-full h-32 sm:w-1/2 sm:h-24 md:w-1/3 md:h-28 lg:w-full"/>
           <div>
             <p className="font-bold">{props.text}</p>
             <br />
