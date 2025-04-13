e4314486d5056294c5bdc957997e73a5754b5f71
dawit2-4 <dawitbirhanu363@gmail.com>
2025-04-13 21:54:02 +0300

    added the header an footer to the homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,8 @@
-<<<<<<< HEAD
-// import Front from "./components/Front";
-// function App() {
-//   return (
-//     <div>
-//       <Front />
-//     </div>
-//   );
-// }
-
-// export default App;
-=======
->>>>>>> 8e562f7c554b383e8d5203202593c36b3fd859c5
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Package from "./pages/Package";
 import Singleblog from "./pages/Singleblog";
-
-
 
 const App = () => {
   return (
@@ -27,7 +12,6 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/package" element={<Package />} />
         <Route path="/blog" element={<Singleblog />} />
-        
       </Routes>
     </Router>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,15 +4,24 @@ import Choose from "../components/Choose";
 import Explore from "../components/Explore";
 import OurClients from "../components/OurClients";
 import Partner from "../components/Partner";
+import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
 
 const Home = () => {
   return (
     <div className="w-full h-screen">
+      <div className="absolute w-full top-0 z-100 bg-transparent backdrop-blur-sm">
+        <NavBar />
+      </div>
       <Banner />
       <Explore />
       <Choose />
       <Partner />
       <OurClients />
+      <div className="hidden sm:block h-12"></div>
+      <div className="mt-20 sm:mt-60 h-auto pb-5">
+        <Footer />
+      </div>
     </div>
   );
 };
